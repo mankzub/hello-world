51d501c1f193ae4ebf9acc7f106a3261c0f2ac4a
mankzub <50206309+mankzub@users.noreply.github.com>
2019-11-21 11:33:21 +0200

    Загрузка файла
    Загрузка файла

---
+++ fan.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="ru">
+<!--<![endif]-->
+  <head> 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NLDV9G7');</script>
+<!-- End Google Tag Manager -->
+ 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+    <meta name="Keywords" content="детский отдых, детский лагерь, детский оздоровительный лагерь, крым, скадовск, аврора, купить путевку, комфорт, безопасность, лагерь на море, лагерь в Крыму, оздоровительный лагерь, летний лагерь, детские лагеря, летние каникулы, детский курорт">
+    <meta name="Description" content="Детский лагерь «Аврора» – летний лагерь, где сбываются мечты. С нами комфортно, интересно и безопасно..">
+    
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <title>Детский лагерь "Аврора" – летний детский отдых на море</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700&amp;subset=cyrillic" rel="stylesheet">
+    
+	
+	<script src="js/jquery-1.6.3.min.js"></script>
+    <script src="js/faq.js"></script>
+    <script>(function(){var a,b,c,d,e,f,g,h,i,j;b=window.device,a={},window.device=a,d=window.document.documentElement,j=window.navigator.userAgent.toLowerCase(),a.ios=function(){return a.iphone()||a.ipod()||a.ipad()},a.iphone=function(){return!a.windows()&&e("iphone")},a.ipod=function(){return e("ipod")},a.ipad=function(){return e("ipad")},a.android=function(){return!a.windows()&&e("android")},a.androidPhone=function(){return a.android()&&e("mobile")},a.androidTablet=function(){return a.android()&&!e("mobile")},a.blackberry=function(){return e("blackberry")||e("bb10")||e("rim")},a.blackberryPhone=function(){return a.blackberry()&&!e("tablet")},a.blackberryTablet=function(){return a.blackberry()&&e("tablet")},a.windows=function(){return e("windows")},a.windowsPhone=function(){return a.windows()&&e("phone")},a.windowsTablet=function(){return a.windows()&&e("touch")&&!a.windowsPhone()},a.fxos=function(){return(e("(mobile;")||e("(tablet;"))&&e("; rv:")},a.fxosPhone=function(){return a.fxos()&&e("mobile")},a.fxosTablet=function(){return a.fxos()&&e("tablet")},a.meego=function(){return e("meego")},a.cordova=function(){return window.cordova&&"file:"===location.protocol},a.nodeWebkit=function(){return"object"==typeof window.process},a.mobile=function(){return a.androidPhone()||a.iphone()||a.ipod()||a.windowsPhone()||a.blackberryPhone()||a.fxosPhone()||a.meego()},a.tablet=function(){return a.ipad()||a.androidTablet()||a.blackberryTablet()||a.windowsTablet()||a.fxosTablet()},a.desktop=function(){return!a.tablet()&&!a.mobile()},a.television=function(){var a;for(television=["googletv","viera","smarttv","internet.tv","netcast","nettv","appletv","boxee","kylo","roku","dlnadoc","roku","pov_tv","hbbtv","ce-html"],a=0;a<television.length;){if(e(television[a]))return!0;a++}return!1},a.portrait=function(){return window.innerHeight/window.innerWidth>1},a.landscape=function(){return window.innerHeight/window.innerWidth<1},a.noConflict=function(){return window.device=b,this},e=function(a){return-1!==j.indexOf(a)},g=function(a){var b;return b=new RegExp(a,"i"),d.className.match(b)},c=function(a){var b=null;g(a)||(b=d.className.replace(/^\s+|\s+$/g,""),d.className=b+" "+a)},i=function(a){g(a)&&(d.className=d.className.replace(" "+a,""))},a.ios()?a.ipad()?c("ios ipad tablet"):a.iphone()?c("ios iphone mobile"):a.ipod()&&c("ios ipod mobile"):a.android()?c(a.androidTablet()?"android tablet":"android mobile"):a.blackberry()?c(a.blackberryTablet()?"blackberry tablet":"blackberry mobile"):a.windows()?c(a.windowsTablet()?"windows tablet":a.windowsPhone()?"windows mobile":"desktop"):a.fxos()?c(a.fxosTablet()?"fxos tablet":"fxos mobile"):a.meego()?c("meego mobile"):a.nodeWebkit()?c("node-webkit"):a.television()?c("television"):a.desktop()&&c("desktop"),a.cordova()&&c("cordova"),f=function(){a.landscape()?(i("portrait"),c("landscape")):(i("landscape"),c("portrait"))},h=Object.prototype.hasOwnProperty.call(window,"onorientationchange")?"orientationchange":"resize",window.addEventListener?window.addEventListener(h,f,!1):window.attachEvent?window.attachEvent(h,f):window[h]=f,f(),"function"==typeof define&&"object"==typeof define.amd&&define.amd?define(function(){return a}):"undefined"!=typeof module&&module.exports?module.exports=a:window.device=a}).call(this);
+</script>
+</head>
+<style>
+HTML,body { max-width: 600x;}
+h1 {
+
+	}
+span { display: none; }
+/* Smartphone Portrait and Landscape */
+@media only screen
+and (min-device-width : 320px)
+and (max-device-width : 480px){ span { display: inline-block; font-size: 14px;}
+}
+.logo {
+    width: 75px;
+    height: 150px; 
+    background-size: 75px 102px;
+    margin-left:230px;
+	margin-top: 30px;   	
+   }
+ 
+ @media only screen
+and (min-device-width : 500px)
+and (max-device-width : 1000px){
+	.logo {
+    width: 75px;
+    height: 150px; 
+    background-size: 75px 102px;
+    margin-left: 20px;
+	margin-top: 10px;   	
+   } 
+  
+    ol {
+	list-style-type: none; /*Отменяет маркеры для списка*/
+    list-style-position: outside;
+	margin-left: 300px; /* Обнуляем значение отступов */
+    margin-bottom: 35px; /*  отступ между заголовком и текстом */
+	
+	
+   }
+   ol li {
+	list-style-type: disc; /* маркер*/
+    padding-top: 10px; /* расстояние между маркерами */
+	margin-left: -335px; /* Обнуляем значение отступов */
+   
+   } 
+
+   li::before {
+    
+    padding-right: 1em; /* Расстояние от маркеров до текста */
+    
+   } 
+}
+ @media only screen
+and (min-device-width : 320px)
+and (max-device-width : 480px){
+	.logo {
+    width: 75px;
+    height: 150px; 
+    background-size: 75px 102px;
+    margin-left:30px;
+	margin-top: 10px;   	
+   } 
+ }
+    h1 {
+    margin-bottom: 35px; /*  отступ между заголовком и текстом */
+   }
+
+
+
+   
+ol {
+	list-style-type: none; /*Отменяет маркеры для списка*/
+    list-style-position: outside;
+	margin-left: 430px; /* Обнуляем значение отступов */
+    margin-bottom: 35px; /*  отступ между заголовком и текстом */
+	
+	
+   }
+ol li {
+	list-style-type: disc; /* маркер*/
+    padding-top: 10px; /* расстояние между маркерами */
+	
+   
+   } 
+
+li::before {
+    
+    padding-right: 1em; /* Расстояние от маркеров до текста */
+    
+   } 
+ 
+ 
+a.nubex {
+	color: #333333;
+	user-select: none; /* Убираем текстовое выделение */
+	text-decoration: none;	/* Убираем подчеркивание */
+	outline: none; /* Убираем контур вокруг ссылки */
+	background-color: #ffde16; /* Цвет фона */
+	padding: 1em 2em; /* Отступ от текста до рамки */
+	border-radius: 75px; /* Уголки */
+    font-size: 20px;
+	}
+	a.nubex:active {
+	background-color: #11d711; /* Цвет кнопки при нажатии */
+	}
+	a.nubex:hover {
+	background-color: #11d711; /* Цвет кнопки при наведении курсора */
+	}
+.footer center {
+   padding: 5em; /* Расстояние от текста до рамки кнопки */
+}     
+   
+   
+   
+ @media only screen
+and (min-device-width : 320px)
+and (max-device-width : 480px){  
+.content { margin-top: -40px;}
+.content p {
+display: none;}  
+.content ol {
+display: none;}
+ }
+@media only screen
+and (min-device-width : 320px)
+and (max-device-width : 480px){  
+
+ ul {
+    list-style-position: inside;
+	margin: -20px; /* Обнуляем значение отступов */
+    margin-bottom: 25px; /*  отступ между заголовком и текстом */
+   }
+ ul li {
+    padding-top: 10px; /* расстояние между маркерами */
+	margin-right: 1px; /* Отступ слева */
+   } 
+ li {
+    
+    text-indent: 0em; /* Сдвигаем элементы списка влево */
+	
+   }
+ li::before {
+    
+    padding-right: 1em; /* Расстояние от маркеров до текста */
+    
+   }
+  a.nubex {
+	color: #333333;
+	user-select: none; /* Убираем текстовое выделение */
+	text-decoration: none;	/* Убираем подчеркивание */
+	outline: none; /* Убираем контур вокруг ссылки */
+	background-color: #ffde16; /* Цвет фона */
+	padding: 1.3em 2em; /* Отступ от текста до рамки */
+	border-radius: 75px; /* Уголки */
+    font-size: 18px;
+	}
+	a.nubex:active {
+	background-color: #11d711; /* Цвет кнопки при нажатии */
+	}
+	a.nubex:hover {
+	background-color: #11d711; /* Цвет кнопки при наведении курсора */
+	} 
+.footer center {
+padding: 4em; /* Расстояние от текста до рамки кнопки */
+}  
+</style>
+</head>
+<body> 
+
+<div class="logo"
+<a href="http://lp.auroracamp.com/" ><img src="img/logo_old.png" alt=""></a>
+</div>	
+	
+<div id="#center"><div class="content">
+    <div class="content">
+	    
+	     <center><h1>Условия акции</h1> </center>
+		 
+       
+		
+		<ol>
+         <p><li>Стоимость путевки в акцию <b>-20%</b> от полной стоимости путевки</li></p>
+         <p><li>Акция действует на первые <b>100 путевок</b> каждой из 5 смен</li></p> 
+         <p><li>Возможна оплата <b>частями</b> см. ниже</li></p> 
+         <p><li>Возможен <b>обмен</b> акционной путевки см. ниже</li></p> 
+         <p><li>Возможен <b>возврат</b> акционной путевки см. ниже</li></p>
+		 </ol>
+	     <ul>
+        <span><li>Стоимость путевки в акцию <b>-20%</b> от полной стоимости путевки</li></span>
+		<span><li>Акция действует на первые <b>100 путевок</b> каждой из 5 смен</li></span>
+		<span><li>Возможна оплата <b>частями</b> см. ниже</li></span>
+		<span><li>Возможен <b>обмен</b> акционной путевки см. ниже</li></span>
+		<span><li>Возможен <b>возврат</b> акционной путевки см. ниже</li></span>
+		</ul>
+
+
+        <center><h1>Оплата частями</h1> </center>
+		
+		
+        <ol>
+         <p><li>Первая оплата не менее <b>30%</b> акционной стоимости путевки</li></p>
+         <p><li>Остаток стоимости путевки оплачивается одним или<br>
+         несколькими платежами <b>до 20.12.2019</b></li></p>
+		 <p><li>При финальной оплате, <b>если менялась стоимость</b> путевки,<br>
+		 производится <b>перерасчет</b> суммы платежа с учетом текущей<br>
+         полной стоимости -20%</li></p>
+		 </ol>
+		 <ul>
+		<span><li>Первая оплата не менее <b>30%</b> акционной стоимости путевки</li></span>
+		<span><li>Остаток стоимости путевки оплачивается одним или несколькими платежами <b>до 20.12.2019</b></li></span>
+		<span><li>При финальной оплате, <b>если менялась стоимость</b> путевки, производится <b>перерасчет</b> суммы платежа с учетом текущей полной стоимости -20%</li></span>
+         </ul>
+		
+		 
+		 
+		 
+		 
+	    <center><h1>Обмен акционной путевки</h1> </center>
+		
+		
+		<ol>
+         <p><li>Возможен не позднее <b>30 дней</b> до дня начала лагерной смены,<br>		
+         при условии наличия свободных мест на выбранную<br> Вами смену</li></p>
+		 <p><li>Производится <b>перерасчет</b> стоимости с учетом оплаченной Вами<br>	
+		 суммы и полной стоимости путевки -20% выбранной Вами<br>
+		 смены на момент оплаты</li></p>
+         <p><li>Возвращаем Вам <b>разницу</b> или Вы доплачиваете  разницу<br> 
+		 по итогу перерасчета</li></p>
+        </ol> 
+		 <ul>
+		 <span><li>Возможен не позднее <b>30 дней</b> до дня начала лагерной смены,при условии наличия свободных мест на выбранную Вами смену</li></span>
+		 <span><li>Производится <b>перерасчет</b> стоимости с учетом оплаченной Вами суммы и полной стоимости путевки -20% выбранной Вами смены на момент оплаты</li></span>
+		 <span><li>Возвращаем Вам разницу или Вы доплачиваете <b>разницу</b> по итогу перерасчета</li></span>
+         </ul>
+		 
+        <center><h1>Возврат акционной путевки</h1></center>
+		
+		
+		<ol>
+         <p><li>Путевки приобретенные в акцию возможно вернуть, так же<br>легко
+		 как путевку приобретенную за полную стоимость,<br> не позднее чем <b>за 5 рабочих
+		 дней</b> до начала лагерной смены</li></p>
+         <p><li>Eсли Вы, по каким либо причинам, пожелаете вернуть путевку<br>
+		 менее чем за 5 рабочих дней до дня начала лагерной смены,мы<br>
+		 постараемся сделать так чтоб Вы не потеряли деньги,<br>
+		 используя <b>список ожидания</b> в котором контакты желающих<br>
+		 попасть на смену на которую у Вас приобретена путевка.</li></p>
+         </ol>
+		 <ul>
+	    <span><li>Путевки приобретенные в акцию возможно вернуть так же легко как путевку приобретенную за полную стоимость, не позднее чем <b>за 5 рабочих дней</b> до начала лагерной смены</li></span>	    
+		<span><li>Eсли Вы,по каким либо причинам, пожелаете вернуть путевку менее чем за 5 рабочих дней до дня начала лагерной смены, мы постараемся сделать так чтоб Вы не потеряли деньги, используя <b>список ожидания</b> в котором контакты желающих попасть на смену на которую у Вас приобретена путевка.</li></span>
+         </ul>
+		</div>
+</div>
+
+	
+	  
+<div class="footer">
+<center><a href="http://www.auroracamp.com/buy.aspx" class="nubex"><b>ВЕРНУТЬСЯ</b></a></center>
+</div>
+</div>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLDV9G7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+</body>
+</html>
+
+
